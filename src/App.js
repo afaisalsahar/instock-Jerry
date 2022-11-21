750ac24d4853296bf71cc47c8164315349dc6395
afaisalsahar <afaisalsahar@gmail.com>
2022-11-21 16:05:04 -0500

    add font face

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import './App.scss'
+
 // import environment variables
 const PORT = process.env.REACT_APP_PORT;
 const URL = process.env.REACT_APP_URL;
 
+
 function App() {
   return (
-    <></>
+    <>
+    <h1>test</h1>
+    </>
   );
 }
 
