3a3151f3dce6c85dbb11c0a035e7c37eb7b2289e
johnwilly990 <j.kim1940@gmail.com>
2022-11-21 21:02:01 -0500

    Add basic route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 // import environment variables
 const PORT = process.env.REACT_APP_PORT;
 const URL = process.env.REACT_APP_URL;
 
 function App() {
   return (
-    <>
-      <h1>test</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
