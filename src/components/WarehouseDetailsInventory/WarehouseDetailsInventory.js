2ebe6ac5faedcd7939f8ed5d83af53f2b66cc725
johnwilly990 <j.kim1940@gmail.com>
2022-11-24 19:59:47 -0500

    Add details component for inventory of the selected warehouse

--- src/components/WarehouseDetailsInventory/WarehouseDetailsInventory.js
+++ src/components/WarehouseDetailsInventory/WarehouseDetailsInventory.js
@@ -6,56 +6,58 @@ import "./WarehouseDetailsInventory.scss";
 
 const WarehouseDetailsInventory = () => {
   return (
-    <div className="inventory__container">
-      <div className="inventory__container-row">
-        <div className="inventory__container-text">
-          <h4 className="inventory__text">inventory item</h4>
+    <section className="inventory">
+      <div className="inventory__container">
+        <div className="inventory__container-sub">
+          <div className="inventory__container-details">
+            <h4 className="inventory__header">inventory</h4>
+            <Link className="inventory__container-chevron">
+              <p className="inventory__text inventory__text--blue">
+                television
+              </p>
+              <img
+                src={chevronArrowIcon}
+                alt="chevron arrow"
+                className="inventory__image"
+              />
+            </Link>
+          </div>
+          <div className="inventory__container-details">
+            <h4 className="inventory__header">category</h4>
+            <p className="inventory__text inventory__text--hidden">
+              electronics
+            </p>
+          </div>
         </div>
-        <div className="inventory__container-text">
-          <h4 className="inventory__text">status</h4>
+        <div className="inventory__container-sub">
+          <div className="inventory__container-details">
+            <h4 className="inventory__header">status</h4>
+            <div className="inventory__container-stock inventory__container-stock--instock inventory__container-stock--outstock">
+              <p className="inventory__stock inventory__stock--instock inventory__stock--outstock">
+                in stock
+              </p>
+            </div>
+          </div>
+          <div className="inventory__container-details">
+            <h4 className="inventory__header">quantity</h4>
+            <p className="inventory__text">500</p>
+          </div>
+        </div>
+
+        <div className="inventory__container-icons">
+          <Link className="inventory__link">
+            <img src={deleteIcon} alt="delete" className="inventory__image" />
+          </Link>
+          <Link className="inventory__link inventory__link--edit">
+            <img
+              src={editIcon}
+              alt="edit pencil"
+              className="inventory__image"
+            />
+          </Link>
         </div>
       </div>
-      <div className="inventory__container-row">
-        <Link className="inventory__link">
-          <h3 className="inventory__item inventory__item--highlight">
-            television
-          </h3>
-          <img
-            src={chevronArrowIcon}
-            alt="chevron arrow"
-            className="inventory__image"
-          />
-        </Link>
-        <div className="inventory__container-text">
-          <p className="inventory__text inventory__text--highlight">in stock</p>
-        </div>
-      </div>
-      <div className="inventory__container-row">
-        <div className="inventory__container-text">
-          <h4 className="inventory__text">category</h4>
-        </div>
-        <div className="inventory__container-text">
-          <h4 className="inventory__text">quantity</h4>
-        </div>
-      </div>
-      <div className="inventory__container-row">
-        <div className="inventory__container-text">
-          <p className="inventory__item">electronics</p>
-        </div>
-        <div className="inventory__container-text">
-          <p className="inventory__text">500</p>
-        </div>
-      </div>
-      <div className="inventory__container-row">
-        <h4 className="inventory__text inventory__text--hidden">actions</h4>
-        <Link className="inventory_details__link">
-          <img src={deleteIcon} alt="delete" className="inventory__image" />
-        </Link>
-        <Link className="inventory__details__link">
-          <img src={editIcon} alt="edit" className="inventory__image" />
-        </Link>
-      </div>
-    </div>
+    </section>
   );
 };
 
