84cc828c0392609d0cb2e1aff796b09186d697dd
Hasan Kandil <hasokandil@gmail.com>
2022-11-24 23:25:01 -0500

    styling inventory list

--- src/components/InventoryCard/InventoryCard.js
+++ src/components/InventoryCard/InventoryCard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import editLogo from '../../assets/images/Icons/edit-24px.svg';
+import editLogo from '../../assets/images/Icons/edit_blue-24px.svg';
 import deleteLogo from '../../assets/images/Icons/delete_outline-24px.svg';
 import chevronLogo from '../../assets/images/Icons/chevron_right-24px.svg';
 import './InventoryCard.scss'
@@ -10,11 +10,10 @@ function InventoryCard({ inventoryList }) {
         inventoryList.map((inventory) => {
             return(
         
-            <div>
-                 <div className='inventory-card' >
+                 <div key={inventory.id} className='inventory-card' >
                      <div className='inventory-card__container'>
                          <h3 className='inventory-card__title inventory-card__title--hidden'>INVENTORY ITEM</h3>
-                         <Link to={'/inventory/' + inventory.id} className='inventory-card__link'>{inventory.item_name}<img src={chevronLogo} alt=''/></Link>
+                         <Link to={'/inventories/' + inventory.id} className='inventory-card__link'>{inventory.item_name}<img src={chevronLogo} alt='Chevron Logo'/></Link>
                      </div>
                       <div className='inventory-card__container'>
                          <h3 className='inventory-card__title inventory-card__title--hidden'>STATUS</h3>
@@ -28,17 +27,16 @@ function InventoryCard({ inventoryList }) {
                          <h3 className='inventory-card__title inventory-card__title--hidden'>QTY</h3>
                          <p className='inventory-card__paragraph'>{inventory.quantity}</p>
                      </div>
-                     {/* <div className='inventory-card__container inventory-card__container--margin'>
+                     <div className='inventory-card__container inventory-card__container--margin'>
                          <h3 className='inventory-card__title inventory-card__title--hidden'>WAREHOUSE</h3>
-                         <p className='inventory-card__paragraph'>{inventory.warehouses.warehouse_name}</p>
-                     </div> */}
-                    {/* <div className='inventory-card__container inventory-card__container--buttons'>
-                         <button onClick={this.clickHandler} className='inventory-card__button btn-openModal'><img src={deleteLogo} alt=''/></button>
-                         <Link className='inventory-card__button' to={'/inventory/edit/' + inventory.id}><img src={editLogo} alt=''/></Link>
-                     </div> */}
+                         <p className='inventory-card__paragraph'>{inventory.warehouse_name}</p>
+                     </div>
+                    <div className='inventory-card__container inventory-card__container--buttons'>
+                         <button onClick className='inventory-card__button btn-openModal'><img src={deleteLogo} alt='Delete Logo'/></button>
+                         <Link className='inventory-card__button' to={'/inventories/edit/' + inventory.id}><img src={editLogo} alt='Edit Logo'/></Link>
+                     </div> 
                  </div>
                 
-            </div>
         );
     })
     )
