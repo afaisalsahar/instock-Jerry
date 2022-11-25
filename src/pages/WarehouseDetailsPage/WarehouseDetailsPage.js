7508bc4750b66cd435386434a6feb28a057c2156
johnwilly990 <j.kim1940@gmail.com>
2022-11-24 19:57:48 -0500

    Add headers for tablet/desktop viewport and its respective styling

--- src/pages/WarehouseDetailsPage/WarehouseDetailsPage.js
+++ src/pages/WarehouseDetailsPage/WarehouseDetailsPage.js
@@ -1,34 +1,77 @@
 import { Link } from "react-router-dom";
 import WarehouseDetails from "../../components/WarehouseDetails/WarehouseDetails";
+import WarehouseDetailsInventory from "../../components/WarehouseDetailsInventory/WarehouseDetailsInventory";
 import backArrow from "../../assets/images/Icons/arrow_back-24px.svg";
 import edit from "../../assets/images/Icons/edit_white-24px.svg";
 import "./WarehouseDetailsPage.scss";
+import sortIcon from "../../assets/images/Icons/sort-24px.svg";
+import { Fragment } from "react";
 
 const WarehouseDetailsPage = () => {
   return (
-    <section className="warehouse-details">
-      <div className="warehouse-details__container-headers">
-        <div className="warehouse-details__container-header">
-          <Link to="/" className="warehouse-details__arrow">
+    <Fragment>
+      <section className="warehouse-details">
+        <div className="warehouse-details__container-headers">
+          <div className="warehouse-details__container-header">
+            <Link to="/" className="warehouse-details__arrow">
+              <img
+                src={backArrow}
+                alt="back arrow"
+                className="warehouse-details__arrow-image"
+              />
+            </Link>
+            <h1 className="warehouse-details__title">Washington</h1>
+          </div>
+          <Link to="/edit-warehouse/:id" className="warehouse-details__edit">
             <img
-              src={backArrow}
-              alt="back arrow"
-              className="warehouse-details__arrow-image"
+              src={edit}
+              alt="edit pencil"
+              className="warehouse-details__edit-img"
             />
+            <p className="warehouse-details__edit-text">Edit</p>
           </Link>
-          <h1 className="warehouse-details__title">Washington</h1>
         </div>
-        <Link to="/edit-warehouse/:id" className="warehouse-details__edit">
-          <img
-            src={edit}
-            alt="edit pencil"
-            className="warehouse-details__edit-img"
-          />
-          <p className="warehouse-details__edit-text">Edit</p>
-        </Link>
-      </div>
-      <WarehouseDetails />
-    </section>
+        <WarehouseDetails />
+        <div className="warehouse-details__container-headings">
+          <div className="warehouse-details__container-heading">
+            <h4 className="warehouse-details__headers">inventory</h4>
+            <img
+              src={sortIcon}
+              alt="sort"
+              className="warehouse-details__sort"
+            />
+          </div>
+          <div className="warehouse-details__container-heading ">
+            <h4 className="warehouse-details__headers">category</h4>
+            <img
+              src={sortIcon}
+              alt="sort"
+              className="warehouse-details__sort"
+            />
+          </div>
+          <div className="warehouse-details__container-heading ">
+            <h4 className="warehouse-details__headers">status</h4>
+            <img
+              src={sortIcon}
+              alt="sort"
+              className="warehouse-details__sort"
+            />
+          </div>
+          <div className="warehouse-details__container-heading warehouse-details__container-heading--padded">
+            <h4 className="warehouse-details__headers">quantity</h4>
+            <img
+              src={sortIcon}
+              alt="sort"
+              className="warehouse-details__sort"
+            />
+          </div>
+          <div className="warehouse-details__container-heading warehouse-details__container-heading--end">
+            <h4 className="warehouse-details__headers">action</h4>
+          </div>
+        </div>
+      </section>
+      <WarehouseDetailsInventory />
+    </Fragment>
   );
 };
 
