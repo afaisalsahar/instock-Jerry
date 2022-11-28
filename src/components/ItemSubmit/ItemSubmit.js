13dbc2e90536dd1775d3995758f8d727be52ef5f
johnwilly990 <j.kim1940@gmail.com>
2022-11-28 16:26:28 -0500

    Add path in cancel link

--- src/components/ItemSubmit/ItemSubmit.js
+++ src/components/ItemSubmit/ItemSubmit.js
@@ -5,7 +5,9 @@ const ItemSubmit = () => {
   return (
     <div className="inventory-add__container-buttons">
       <div className="inventory-add__container-cancel">
-        <Link className="inventory-add__cancel">Cancel</Link>
+        <Link to="/inventories" className="inventory-add__cancel">
+          Cancel
+        </Link>
       </div>
       <div className="inventory-add__container-add">
         <button className="inventory-add__button">+ Add Item</button>
